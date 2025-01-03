9b41d46d98d5e93eaa23c31a0c172c1040580e8a
Wathsalya Gamage <wathsalyagamage23@gmail.com>
2025-01-03 13:59:26 +0530

    added CI/CD pipeline

--- Project/cucumber.js
+++ Project/cucumber.js
@@ -1,19 +1,17 @@
 module.exports = {
-   default: {
-      paths: ['src/ui/features/', 'src/api/features/'],
-      require: ['src/ui/step_definitions/*.js', 'src/api/step_definitions/*.js'],
-      format: ['html:reports/cucumber-report.html', 'json:reports/cucumber-report.json', 'summary'],
-      formatOptions: { snippetInterface: 'async-await' },
-      timeout: 60000
-   },
-   ui: {
-      paths: ['src/ui/features/'],
-      require: ['src/ui/step_definitions/*.js'],
-      format: ['html:reports/ui-cucumber-report.html', 'json:reports/ui-cucumber-report.json', 'summary']
-   },
-   api: {
-      paths: ['src/api/features/'],
-      require: ['src/api/step_definitions/*.js'],
-      format: ['html:reports/api-cucumber-report.html', 'json:reports/api-cucumber-report.json', 'summary']
-   }
+  default: '--publish-quiet',
+  ui: [
+    '--require-module @babel/register',
+    '--require src/ui/step_definitions/**/*.js',
+    '--format json:reports/ui-cucumber-report.json',
+    '--format progress',
+    'src/ui/features/**/*.feature'
+  ].join(' '),
+  api: [
+    '--require-module @babel/register',
+    '--require src/api/step_definitions/**/*.js',
+    '--format json:reports/api-cucumber-report.json',
+    '--format progress',
+    'src/api/features/**/*.feature'
+  ].join(' ')
 };
