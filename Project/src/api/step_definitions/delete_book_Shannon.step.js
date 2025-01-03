702653142252ac01c32e0e5a7f04d7ecea3e3c18
Wathsalya Gamage <wathsalyagamage23@gmail.com>
2025-01-03 22:31:07 +0530

    Updated

--- Project/src/api/step_definitions/delete_book_Shannon.step.js
+++ Project/src/api/step_definitions/delete_book_Shannon.step.js
@@ -1,6 +1,6 @@
 const { Given, When, Then, After } = require('@cucumber/cucumber');
 const { request, expect } = require('@playwright/test');
-const { BASE_URL, context, createAuthHeader } = require('./consts');
+const { BASE_URL, context, createAuthHeader } = require('./common');
 
 Given('I am an authorized user', async function () {
    context.apiContext = await request.newContext({
--- Project/src/api/step_definitions/getAllBooks_Sanduni.step.js
+++ Project/src/api/step_definitions/getAllBooks_Sanduni.step.js
@@ -1,56 +1,58 @@
 const { Given, When, Then, After } = require('@cucumber/cucumber');
 const { request, expect } = require('@playwright/test');
-const { BASE_URL, context, createAuthHeader } = require('./consts');
+const { BASE_URL, context, createAuthHeader } = require('./common');
 
-Given('I am logged in as {string} user', async function (userType) {
-   context.apiContext = await request.newContext({
-      baseURL: BASE_URL,
-      extraHTTPHeaders: {
-         'Authorization': createAuthHeader(userType, 'password')
-      }
-   });
+let response;
+
+Given('I am logged in as {string} user', async function(userType) {
+    context.apiContext = await request.newContext({
+        baseURL: BASE_URL,
+        extraHTTPHeaders: {
+            'Authorization': createAuthHeader(userType, 'password')
+        }
+    });
 });
 
-Given('I am not authenticated', async function () {
-   context.apiContext = await request.newContext({
-      baseURL: BASE_URL
-   });
+Given('I am not authenticated', async function() {
+    context.apiContext = await request.newContext({
+        baseURL: BASE_URL
+    });
 });
 
-When('I send GET request to {string}', async function (endpoint) {
-   response = await context.apiContext.get(endpoint);
+When('I send GET request to {string}', async function(endpoint) {
+    response = await context.apiContext.get(endpoint);
 });
 
-Then('the response status should be {int}', async function (statusCode) {
-   expect(response.status()).toBe(statusCode);
+Then('the response status should be {int}', async function(statusCode) {
+    expect(response.status()).toBe(statusCode);
 });
 
-Then('the response should contain the list of books', async function () {
-   const books = await response.json();
-   expect(Array.isArray(books)).toBeTruthy();
-   if (books.length > 0) {
-      const firstBook = books[0];
-      expect(firstBook).toHaveProperty('id');
-      expect(firstBook).toHaveProperty('title');
-      expect(firstBook).toHaveProperty('author');
-   }
+Then('the response should contain the list of books', async function() {
+    const books = await response.json();
+    expect(Array.isArray(books)).toBeTruthy();
+    if (books.length > 0) {
+        const firstBook = books[0];
+        expect(firstBook).toHaveProperty('id');
+        expect(firstBook).toHaveProperty('title');
+        expect(firstBook).toHaveProperty('author');
+    }
 });
 
-Then('the response should contain an empty list', async function () {
-   const books = await response.json();
-   expect(Array.isArray(books)).toBeTruthy();
+Then('the response should contain an empty list', async function() {
+    const books = await response.json();
+    expect(Array.isArray(books)).toBeTruthy();
 });
 
-When('I send a GET request to {string}', async function (endpoint) {
-   response = await context.apiContext.get(endpoint);
+When('I send a GET request to {string}', async function(endpoint) {
+    response = await context.apiContext.get(endpoint);
 });
 
-Then('the response status code for getAllBooks should be {int}', async function (statusCode) {
-   expect(response.status()).toBe(statusCode);
+Then('the response status code for getAllBooks should be {int}', async function(statusCode) {
+    expect(response.status()).toBe(statusCode);
 });
 
-After(async function () {
-   if (context.apiContext) {
-      await context.apiContext.dispose();
-   }
+After(async function() {
+    if (context.apiContext) {
+        await context.apiContext.dispose();
+    }
 });
